f9ab5f0b3ab3451d1b4a93681bbd8d2530437e41
Meric Dagli <mericda@gmail.com>
2017-09-13 20:23:29 -0400

    v1.1,4

--- app.rb
+++ app.rb
@@ -70,3 +70,13 @@ get '/meeting_times' do
   <li>use <b>/class</b> to get the course schedule.</li>
   <li>use <b>/officehours</b> to get Daragh's office hours.</li></ul>"
 end
+
+get '/meeting_times/:item' do
+  if params["item"] == 'class'
+    "Course meets at 3-5pm on Tuesdays and Thursdays. "
+  elsif params["item"] == 'officehours'
+    "Daragh is available for any questions between 5:00 to 6:30 on Fridays"
+  else
+    redirect to ('/meeting_times')
+  end
+  end
